8aedc7896750ef8e07a9feb287f30fb07e62ef5f
rbndg <rbandegi@gmail.com>
2019-11-10 09:28:18 +1100

    Larger tx size

--- rpcserver.go
+++ rpcserver.go
@@ -63,7 +63,7 @@ import (
 const (
 	// maxBtcPaymentMSat is the maximum allowed Bitcoin payment currently
 	// permitted as defined in BOLT-0002.
-	maxBtcPaymentMSat = lnwire.MilliSatoshi(math.MaxUint32)
+	maxBtcPaymentMSat = lnwire.MilliSatoshi(math.MaxUint32) * 12
 
 	// maxLtcPaymentMSat is the maximum allowed Litecoin payment currently
 	// permitted.
